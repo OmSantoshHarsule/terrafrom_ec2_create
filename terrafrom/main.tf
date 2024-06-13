e360e31ac3d57fa0d3320d17bf675fe61eb3f17c
Om Harsule <om.harsule.11@gmail.com>
2024-06-13 23:36:27 +0530

    my first commit

---
+++ terrafrom/main.tf
@@ -0,0 +1,24 @@
+data "aws_ami" "ubuntu" { 
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-20240423"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0f58b397bc5c1f2e8"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
