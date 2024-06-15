b3b90b8dee665a2efd24a29c5f080970148497ae
Om Harsule <om.harsule.11@gmail.com>
2024-06-15 13:30:05 +0530

    dynomodb table

--- terrafrom/remote_state/main.tf
+++ terrafrom/remote_state/main.tf
@@ -8,3 +8,25 @@ resource "aws_s3_bucket" "example" {
   }
 }
 
+#################### create dyanomo_db_table #####################
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name           = "terraform-lock-table"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "terraform-lock-table"
+    Environment = "production"
+  }
+}
+
